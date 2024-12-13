c84935fe94f92a77be9abcaf03e4313237f8d360
dheeraj.singh@capgemini.com <dheeraj.singh@capgemini.com>
2024-12-14 02:01:07 +0530

    SellerControllerApiAdded

--- EHSWebAPI/Repositories/SellersRepository/ISellerRepository.cs
+++ EHSWebAPI/Repositories/SellersRepository/ISellerRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace EHSWebAPI.Repositories.SellersRepository
+﻿using EHSDataAccessLayer.Entity;
+using System.Collections;
+using System.Collections.Generic;
+namespace EHSWebAPI.Repositories.SellersRepository
 {
     public interface ISellerRepository
     {
+        IEnumerable<Seller> GetAll();
+        
     }
 }
