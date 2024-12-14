bbb7fd7d75588f67ceab772663a6621f82e9b729
Himanshu Kushwaha <himanshu.kushwaha@capgemini.com>
2024-12-14 17:43:31 +0530

    Added UserApi

--- EHSWebAPI/Repositories/UsersRepository/IUserRepository.cs
+++ EHSWebAPI/Repositories/UsersRepository/IUserRepository.cs
@@ -1,6 +1,15 @@
-﻿namespace EHSWebAPI.Repositories.UsersRepository
+﻿using EHSDataAccessLayer.Entity;
+using System.Collections.Generic;
+
+namespace EHSWebAPI.Repositories.UsersRepository
 {
     public interface IUserRepository
     {
+        IEnumerable<User> GetAllUser();
+        User CreateUser(User user);       
+        bool DeleteUser(string user);
+        User GetUserByUserName(string UserName);
+        bool UserExists(string UserName);
+
     }
 }
--- EHSWebAPI/Repositories/UsersRepository/UserRepository.cs
+++ EHSWebAPI/Repositories/UsersRepository/UserRepository.cs
@@ -1,11 +1,53 @@
-﻿using System;
+﻿using EHSDataAccessLayer.Entity;
+using EHSDataAccessLayer.Entity.Context;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace EHSWebAPI.Repositories.UsersRepository
 {
-    public class UserRepository
+    public class UserRepository : IUserRepository
     {
+        private readonly EHSDbContext _eHSDbContext;
+
+        public UserRepository(EHSDbContext eHSDbContext)
+        {
+            _eHSDbContext = eHSDbContext;
+        }
+
+        public User CreateUser(User user)
+        {
+            _eHSDbContext.Users.Add(user);
+            _eHSDbContext.SaveChanges();
+            return user;
+        }
+
+        public bool DeleteUser(string UserName)
+        {
+            var user = _eHSDbContext.Users.SingleOrDefault(us => us.UserName == UserName);
+            if (user == null) return false;
+
+            _eHSDbContext.Users.Remove(user);
+            _eHSDbContext.SaveChanges();
+            return true;
+        }
+
+        public IEnumerable<User> GetAllUser()
+        {
+            return _eHSDbContext.Users.ToList();
+        }
+
+        public User GetUserByUserName(string UserName)
+        {
+            return _eHSDbContext.Users.SingleOrDefault(name => name.UserName == UserName);
+        }
+
+      
+
+        public bool UserExists(string UserName)
+        {
+            return _eHSDbContext.Users.Any(user => user.UserName == UserName);
+        }
     }
 }
