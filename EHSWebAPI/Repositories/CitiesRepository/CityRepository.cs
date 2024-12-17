81d69f9281b1ad5fbba47b5ee40d1e1b7963a5e4
sbadyal <shammi.badyal@capgemini.com>
2024-12-17 12:59:00 +0530

    added: exceptional handling in cityApi

--- EHSWebAPI/Repositories/CitiesRepository/CityRepository.cs
+++ EHSWebAPI/Repositories/CitiesRepository/CityRepository.cs
@@ -15,45 +15,96 @@ namespace EHSWebAPI.Repositories.CitiesRepository
         {
             _eHSDbContext = eHSDbContext;
         }
+
         public IEnumerable<City> GetAllCities()
         {
-            return _eHSDbContext.Cities.ToList();
+            try
+            {
+                return _eHSDbContext.Cities.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("An error occurred while retrieving all cities.", ex);
+            }
         }
 
         public City GetCityById(int id)
         {
-            return _eHSDbContext.Cities.SingleOrDefault(c => c.CityId == id);
+            try
+            {
+                return _eHSDbContext.Cities.SingleOrDefault(c => c.CityId == id);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"An error occurred while retrieving the city with ID {id}.", ex);
+            }
         }
 
         public IEnumerable<City> GetCitiesByState(int stateId)
         {
-            return _eHSDbContext.Cities.Where(c => c.StateId == stateId).ToList();
+            try
+            {
+                return _eHSDbContext.Cities.Where(c => c.StateId == stateId).ToList();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"An error occurred while retrieving cities for state ID {stateId}.", ex);
+            }
         }
 
         public void AddCity(City city)
         {
-            _eHSDbContext.Cities.Add(city);
-            _eHSDbContext.SaveChanges();
+            try
+            {
+                _eHSDbContext.Cities.Add(city);
+                _eHSDbContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("An error occurred while adding a new city.", ex);
+            }
         }
 
         public void UpdateCity(City city)
         {
-            var existingCity = _eHSDbContext.Cities.SingleOrDefault(c => c.CityId == city.CityId);
-            if (existingCity != null)
+            try
             {
-                existingCity.CityName = city.CityName;
-                existingCity.StateId = city.StateId;
-                _eHSDbContext.SaveChanges();
+                var existingCity = _eHSDbContext.Cities.SingleOrDefault(c => c.CityId == city.CityId);
+                if (existingCity != null)
+                {
+                    existingCity.CityName = city.CityName;
+                    existingCity.StateId = city.StateId;
+                    _eHSDbContext.SaveChanges();
+                }
+                else
+                {
+                    throw new Exception($"City with ID {city.CityId} does not exist.");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"An error occurred while updating the city with ID {city.CityId}.", ex);
             }
         }
 
         public void DeleteCity(int id)
         {
-            var city = _eHSDbContext.Cities.SingleOrDefault(c => c.CityId == id);
-            if (city != null)
+            try
             {
-                _eHSDbContext.Cities.Remove(city);
-                _eHSDbContext.SaveChanges();
+                var city = _eHSDbContext.Cities.SingleOrDefault(c => c.CityId == id);
+                if (city != null)
+                {
+                    _eHSDbContext.Cities.Remove(city);
+                    _eHSDbContext.SaveChanges();
+                }
+                else
+                {
+                    throw new Exception($"City with ID {id} does not exist.");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"An error occurred while deleting the city with ID {id}.", ex);
             }
         }
     }
